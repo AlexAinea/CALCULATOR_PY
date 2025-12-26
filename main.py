6285b6bf8882ade1baf44a6b8ddf1740874d6f37
Alex Ainea <xo.ainea@gmail.com>
2025-12-26 10:45:50 +0300

    button reuse logic done

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 
-
-
 root = tk.Tk()
 root.title("CALCULATOR")
 root.geometry("600x600")
@@ -19,11 +17,18 @@ button_frame.pack()
 
 def buttons():
     numbers = [1,2,3,4,5,6,7,8,9,0]
+    rows = 0
+    
+    columns = 0
     for number in numbers:
-        rows = 0
-        columns = 0
         button = tk.Button(button_frame,text = number)
         button.grid(row = rows ,column = columns)
+
+        columns += 1
+        rem = columns%3
+        if rem == 0:
+            columns = 0
+            rows += 1
 
 buttons()
 
